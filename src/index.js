55f766f54209cd067ba95ebaafe8325b965d182a
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2024-02-25 14:23:14 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
-import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-06-phonebook">
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      <BrowserRouter basename="/goit-react-hw-07-phonebook">
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
